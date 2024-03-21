e4dda7df4f4e617d59dbf6a856dd841ef5cad1ba
Chanchal <114665425+Chanchalsanthosh@users.noreply.github.com>
2024-03-22 00:30:39 +0530

    Update Announcement.jsx

--- src/components/announcement.jsx
+++ src/components/announcement.jsx
@@ -1,44 +1,29 @@
-import React,{ useEffect, useState } from 'react'
-import {firestore} from 'src/firebase';
-import {getDocs,collection} from '@firebase/firestore';
-const announcement = () => {
-  let [Anns,setAnns]=useState([]);
-  const filteredAnns=Anns.filter(Ann=>Ann.id>Anns.length-3);
-  useEffect(() => {
-        const fetchData = async () => {
-            const ref = collection(firestore, 'Announcements');
-            try {
-                const querySnapshot = await getDocs(ref);
-                const newData = querySnapshot.docs.map(doc => doc.data()
-                );
-                const sortedData = newData.sort((a, b) => b.id - a.id);
-                setAnns(sortedData);
-                console.log(anns);
-            } catch (error) {
-                console.error('Error fetching documents: ', error);
-            }
-        };
+import React from 'react'
 
-        fetchData();
-    }, []); // empty dependency array - run the effect only once on component mount, if necessary hooks will be added late
+function Announcement() {
   
   return (
-    <div className='announcement'>
-     
-    <div className='box1'>
-      Announcements
-    </div>
-     <div className='box2'> 
-       <p>
-         {filteredAnns.map((ann, index) => (
-                <div className='preview Blog' key={index}>
-                    <p><h1>{ann.Title}</h1></p>
-                </div>
-            ))}
-       </p>
-     </div>
-    </div> 
+    <div className="news-container">
+            <div className="title">
+                Announcement
+            </div>
+
+            <ul>
+                <li>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam!
+                </li>
+                
+                <li>
+                    Lorem ipsum dolor sit
+                </li>
+
+                <li>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam!
+                </li>
+            </ul>
+        </div>
+
   )
 }
 
-export default announcement
+export default Announcement;
