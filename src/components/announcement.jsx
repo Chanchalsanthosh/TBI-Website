d3a0df30a8a094a60962eb7dd21c0282e1809014
Rimasasikumar <114665075+Rimasasikumar@users.noreply.github.com>
2024-03-21 23:12:45 +0530

    Update announcement.jsx

--- src/components/announcement.jsx
+++ src/components/announcement.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-
+import React,{ useEffect, useState } from 'react'
+import {firestore} from 'src/firebase';
+import {getDocs,collection} from '@firebase/firestore';
 const announcement = () => {
+  let [Anns,setAnns]=useState([]);
+  const filteredAnns=Anns.filter(Ann=>Ann.id>Anns.length-3);
+  useEffect(() => {
+        const fetchData = async () => {
+            const ref = collection(firestore, 'Announcements');
+            try {
+                const querySnapshot = await getDocs(ref);
+                const newData = querySnapshot.docs.map(doc => doc.data()
+                );
+                const sortedData = newData.sort((a, b) => b.id - a.id);
+                setAnns(sortedData);
+                console.log(anns);
+            } catch (error) {
+                console.error('Error fetching documents: ', error);
+            }
+        };
+
+        fetchData();
+    }, []); // empty dependency array - run the effect only once on component mount, if necessary hooks will be added late
+  
   return (
     <div className='announcement'>
      
     <div className='box1'>
       Announcements
     </div>
-     <div className='box2'> .lorem! </div>
+     <div className='box2'> 
+       <p>
+         {filteredAnns.map((ann, index) => (
+                <div className='preview Blog' key={index}>
+                    <p><h1>{ann.Title}</h1></p>
+                </div>
+            ))}
+       </p>
+     </div>
     </div> 
   )
 }
