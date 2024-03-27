7eeab035c08c2e1b24dcb2e75f1e8c2eda5423c1
Chanchal <114665425+Chanchalsanthosh@users.noreply.github.com>
2024-03-27 19:55:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import Header from "./components/header";
 import Home from "./components/home";
 import About from "./components/about";
 import Announcement from "./components/announcement";
+import Company from "./components/company";
+import Blog from "./components/blog";
+import Features from "./components/features";
+import Footer from "./components/Footer";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/about.scss";
 import "./styles/announcement.scss";
+import "./styles/company.scss";
+import "./styles/blog.scss";
+import "./styles/features.scss";
+import "./styles/footer.scss";
 const App = () => {
   return (
     <div>
@@ -14,6 +22,10 @@ const App = () => {
       <Home/>
       <Announcement/>
       <About/>
+     <Features/>
+      <Company/>
+      <Blog/>
+      <Footer/>
       
     </div>
   )
