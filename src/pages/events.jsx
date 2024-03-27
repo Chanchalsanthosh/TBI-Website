5c2a8f8004033603080d500f977a60688348c3d7
Chanchal <114665425+Chanchalsanthosh@users.noreply.github.com>
2024-03-27 19:56:13 +0530

    Update events.jsx

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from "../components/header";
+import Calendar from "../components/Calendar";
+import "../styles/calendar.scss"
 
 const events = () => {
   return (
     <div>
      <Header/> 
      <div>EVENTS</div>
+     <Calendar />
     </div>
   )
 }
