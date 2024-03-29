312e388de9c699954d2c1f8368b63b0ed2ee1da6
Megha Ranjani <114086612+megharanjani@users.noreply.github.com>
2024-03-29 21:05:38 +0530

    Update mentor.jsx

--- src/pages/mentor.jsx
+++ src/pages/mentor.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import Header from "../components/header";
-
+import FlippableCard from '../components/card/flippable-card';
+import FlippableCard2 from '../components/card/flippable-card2';
+import FlippableCard3 from '../components/card/flippable-card3';
+import FlippableCard4 from '../components/card/flippable-card4';
+import '../styles/mentor.scss'
 const mentor = () => {
+  
   return (
+    
     <div>
-      <Header />
-      <div>Mentors</div>
+      <Header/>
+      <h1 className='heading'>Mentors</h1>
+      <div className='card1'>
+      <FlippableCard/>
+      </div>
+      <div className='card2'>
+      <FlippableCard2/>
+      </div>
+      <div className='card3'>
+      <FlippableCard3/>
+      </div>
+      <div className='card4'>
+      <FlippableCard4/>
+      </div>
+      
     </div>
   )
 }
