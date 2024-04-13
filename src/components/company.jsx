203623d55821283e556f0f38d1bbd06dd62f6850
Chanchal <114665425+Chanchalsanthosh@users.noreply.github.com>
2024-04-13 15:45:17 +0530

    Update company.jsx

--- src/components/company.jsx
+++ src/components/company.jsx
@@ -1,19 +1,66 @@
 import React from "react";
 import data from "../assests/dataimg.json";
-const company = () => {
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+//import { useNavigate } from "react-router-dom";
+const Company = () => {
+ // const navigate = useNavigate();
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    autoplay:true,
+    speed:2000,
+    beforeChange: function(currentSlide, nextSlide) {
+      console.log("before change", currentSlide, nextSlide);
+    },
+    afterChange: function(currentSlide) {
+      console.log("after change", currentSlide);
+    }
+  };
   return (
     <div id="company">
       <h3>Our Startups</h3>
       <h1>COMPANIES</h1>
-     
-      {data.Image.map((i) => (
-              <div key={i.id} className="card">
+
+        <div className="slider-container">
+      <Slider {...settings}>
+       
+        {data.Image.map((i) => (
+              <div key={i.id} className="own_card">
+                <div class="left">
                <img src={i.name} alt={"i.name"} />
                 </div>
+                <aside>
+               <div className="ccontent">
+                <h2>{i.cname}</h2>
+                <p>{i.about}</p>
+                <div class="buttons">
+                <a href={i.url}>
+                  <button>Visit Us</button>
+                </a>
+                <a href={i.knowmore}>
+                  <button>Know More</button>
+                </a>
+                 
+                </div>
+               
+               </div>
+               </aside>
+            </div>
          ))}
+       
+
+      </Slider>
       
     </div>
+  </div>
   )
 }
 
-export default company;
+export default Company;
